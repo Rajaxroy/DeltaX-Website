daf63bbaa2ed55daa03a7acbcfc17ac5142a11a7
Raja Roy <rajaroy495@gmail.com>
2022-07-21 02:57:26 +0530

    Complete UI

--- main.py
+++ main.py
@@ -1,23 +1,21 @@
-
 from flask import Flask, render_template, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
-from wtforms import StringField, SelectField, SubmitField,DateField, FileField,TextAreaField
+from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired
-from wtforms import validators
 
-
+from flask_wtf.file import FileField, FileAllowed, FileRequired
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'the random string' 
 Bootstrap(app)
 
 class MyForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired()])
-    date = DateField("Date Released", validators=[DataRequired()])
-    image = SelectField("Image url", validators=[DataRequired()])
-    artist = SelectField('Artist', choices=["Arijit", "Sidhu Mosewala","Guru"])
-    submit = SubmitField("Save")
+    name = StringField('Name ', validators=[DataRequired()])
+    date = StringField("Date Released eg: 'July 17, 2019' ", validators=[DataRequired()])
+    upload = FileField('Upload image ', validators=[FileRequired(), FileAllowed(['jpg', 'png'], 'Images only!')])
+    artist = SelectField('Artist ', choices=["Arijit", "Sidhu Mosewala","Guru"])
+    submit = SubmitField("Save ")
 
 @app.route("/")
 def home():
@@ -27,6 +25,14 @@ def home():
 def add_song():
     form = MyForm()
     return render_template("add.html", form=form)
+
+@app.route("/all-songs")
+def all_songs():
+    return render_template("allSongs.html")
+
+@app.route("/all-artist")
+def all_artists():
+    return render_template("allArtists.html")
 
 
 if __name__ == "__main__":
